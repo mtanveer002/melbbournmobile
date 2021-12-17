5825194830aef1cf0c6ad34e36cb6c441655bd74
Muhammad Tanveer <mtanveerraj002@gmail.com>
2021-12-17 15:32:13 +0500

    live chat

--- resources/views/website/master.blade.php
+++ resources/views/website/master.blade.php
@@ -235,7 +235,8 @@
     </script>
 
    
-
+<script type="text/javascript" id="zsiqchat">var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "7aa6606e19c69247846780e9db9a5e9ab6f3e893573db44a1fc837de6eda0774", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
+</script>
 </body>
 
 </html>
