e72e5c58affa6590673fa5f2a04b4ea29ac01103
Muhammad Tanveer <mtanveerraj002@gmail.com>
2022-01-20 16:14:48 +0500

    add mail signature

--- resources/views/email/quoteMail.blade.php
+++ resources/views/email/quoteMail.blade.php
@@ -1,40 +1,75 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <style>
-        table,
-        th,
-        td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-
-        th,
-        td {
-            padding: 5px;
-            text-align: left;
-        }
-
-    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
 </head>
-
 <body>
-    <h2>Dear Valuable Customer</h2>
-    <h3>Thanks for your inquery (MELBOURNE MOBILE PHONE REPAIRS)</h3>
-    <br>
-    <h3>Reply</h3>
-    <p>{{$userdata['reply']}}</p>
-    <br>
-    <b>Thank You. for choosing MMPR. Form more information please contact us on <br> Phone: (03) 8845 7222 <br> Email: <a href="mmpr.sales@gmail.com">mmpr.sales@gmail.com</a> <br>
-        Website: <a href=" https://melbournemobilephonerepairs.com.au/"> https://melbournemobilephonerepairs.com.au/</a>.</b>
-</body>
+    <style>
+            img{
+                width: 90px;
+                height: 90px;
+            }
+            .data{
+                padding-left: 200px;
+                padding-top: 3%;
+                margin-top: -14%;
+                margin-left: -100px;
+            }
+            .fab{
+                font-size: 2rem;
+            }
+            .fa-instagram-square{
+                color: #8e2e92;
+            }
+            .fa-facebook-square{
+                color: #033e82;
+            }
+            .sincerely{
+                color: #b5723a;
+                font-weight: 600;
+                font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+            }
+            .p1{
+                font-family: sans-serif;
+            }
+            .web{
+                color: black;
+            }
+    </style>
 
+    <p class="sincerely"><i>Sincerely,</i></p>
+    <p class="p1"><i><strong>Steven Parker</strong></i></p>
+    <p class="p1"><i><strong>Customer Services</strong></i></p>
+    <br>
+        <h4>Reply:</h4> <hr>
+        <p> {{$userdata['reply']}}</p>
+    <br>
+    <hr>
+    
+        <div class="bottom">
+            <div class="row">
+                <div class="col-2 left">
+                    <img src="{{asset('backend/images/mmpr_logo.png')}}"/>
+                </div>
+                <div class="data col-10">
+                    Tel: (03) 8845 7222 <br>
+                     Mail: <a href="mmpr.sales@gmail.com">mmpr.sales@gmail.com</a>
+                      <br>
+                      Web: <a class="melbournemobilephonerepairs.com.a" href="#">melbournemobilephonerepairs.com.au/</a>
+                      <br>
+                      Add:  3 Lawrence St, Blackburn South, VIC 3130 82 Somerville Rd, Hampton Park VIC 3976, Australia</p> 
+                      <i class="fab fa-facebook-square"></i>&nbsp;&nbsp;<i class="fab fa-instagram-square"></i>
+                  </div>
+            </div>
+        </div>
+</body>
 </html>
+
+
