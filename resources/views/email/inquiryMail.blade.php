d1c79c489a4e66c355c7af605fe178e9831238c0
Muhammad Tanveer <mtanveerraj002@gmail.com>
2022-01-25 13:29:57 +0500

    add signature of van regis

--- resources/views/email/inquiryMail.blade.php
+++ resources/views/email/inquiryMail.blade.php
@@ -1,15 +1,68 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Malbourne</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0," />
+    <style type="text/css">
+            
+        img{
+            width: 90px;
+            height: 90px;
+        }
+        .data {
+            padding-left: 200px;
+            padding-top: 0%;
+            margin-left: -100px;
+            margin-top: -90px;
+             }
+        .fab{
+            font-size: 2rem;
+        }
+        .fa-instagram-square{
+            color: #8e2e92;
+        }
+        .fa-facebook-square{
+            color: #033e82;
+        }
+        .sincerely{
+            color: #b5723a;
+            font-weight: 600;
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        }
+        .p1{
+            font-family: sans-serif;
+        }
+        .web{
+            color: black;
+        }
+    </style>
+
 </head>
 <body>
-<h2>Dear Valuable Customer</h2>
+  
+    <p class="sincerely"><i>Sincerely,</i></p>
+    <p class="p1"><i><strong>Steven Parker</strong></i></p>
+    <p class="p1"><i><strong>Customer Services</strong></i></p>
     <br>
-    <p>{{ $data }}</p>
+        <h4>Reply:</h4> <hr>
+        <p> {{ $data }}</p>
     <br>
-    <b>Thank You. for choosing MMPR. Form more information please contact us on <br> Phone: (03) 8845 7222 <br> Email: <a href="mmpr.sales@gmail.com">mmpr.sales@gmail.com</a> <br>
-        Website: <a href=" https://melbournemobilephonerepairs.com.au/"> https://melbournemobilephonerepairs.com.au/</a>.</b>
+    <hr>
+    
+        <div class="bottom">
+            <img src="{{asset('backend/images/mmpr_logo.png')}}"/>
+        </div>
+        <div class="data">
+                Tel: (03) 8845 7222 <br>
+                Mail: <a href="mmpr.sales@gmail.com">mmpr.sales@gmail.com</a>
+                <br>
+                Web: <a class="melbournemobilephonerepairs.com.a" href="#">melbournemobilephonerepairs.com.au/</a> <br>
+                Add: 3 Lawrence St, Blackburn South, VIC 3130 82 Somerville Rd, Hampton Park VIC 3976, Australia</p> 
+                <i class="fab fa-facebook-square"></i>&nbsp;&nbsp;<i class="fab fa-instagram-square"></i>
+            </div>
+            </div>
+        </div>
 </body>
 </html>
+
+
