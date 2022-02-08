a1da854908f324ed38adc3df56f04d99516fc890
zeshan <zeshan@thinkagency.com>
2022-02-08 09:02:29 -0500

    fixed migrattion error

--- resources/views/admin/issues/edit.blade.php
+++ resources/views/admin/issues/edit.blade.php
@@ -14,46 +14,10 @@
                     @csrf
                     @method('put')
                     <div class="mb-1">
-                        <label for="brand" class="form-label">Brand</label>
-                        <select class="form-select-sm form-control"  name="brand" required>
-                            @foreach ($brands as $brand)
-                                <option value="{{$brand->id}}">{{$brand->brand}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    @error('brand')
-                        <div class="text-danger">{{$message}}</div>
-                    @enderror
-                    <div class="mb-1">
-                        <label for="modal" class="form-label">Modals</label>
-                        <select class="form-select-sm form-control"  name="modal" required>
-                             @foreach ($modals as $modal)
-                                <option value="{{$modal->id}}">{{$modal->name}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    @error('modal')
-                        <div class="text-danger">{{$message}}</div>
-                    @enderror
-                    <div class="mb-1">
                     <label for="issue" class="form-label">Issue</label>
                     <input type="text" class="form-control" id="modal" name="issue" value="{{$issues->issue}}" required>
                     </div>
                     @error('issue')
-                        <div class="text-danger">{{$message}}</div>
-                    @enderror
-                    <div class="mb-1">
-                        <label for="price" class="form-label">Price</label>
-                        <input type="number" class="form-control" id="price" name="price" value="{{$issues->price}}">
-                    </div>
-                    @error('price')
-                        <div class="text-danger">{{$message}}</div>
-                    @enderror
-                    <div class="mb-1">
-                        <label for="Description" class="form-label">Description</label>
-                        <input type="text" class="form-control" id="description" name="description" value="{{$issues->description}}">
-                    </div>
-                    @error('description')
                         <div class="text-danger">{{$message}}</div>
                     @enderror
                     <button type="submit" class="btn btn-success float-right ml-1">Update Issue</button>
@@ -67,29 +31,6 @@
 </div>
 
 {{-- for auto selection select box --}}
-<script type="text/javascript">
-    $(document).ready(function() {
-      $('select[name="brand"]').on('change', function(){
-          var brand = $(this).val();
-          if(brand) {
-              $.ajax({
-                  url: "{{  url('/admin/modal/ajax') }}/"+brand,
-                  type:"GET",
-                  dataType:"json",
-                  success:function(data) {
-                     var d =$('select[name="modal"]').empty();
-                        $.each(data, function(key, value){
-                            $('select[name="modal"]').append('<option value="'+ value.id +'">' + value.name + '</option>');
-                        });
-                  },
-              });
-              console.log(brand)
-          } else {
-              alert('danger');
-          }
-      });
-  });
-  </script>
 
 
 @endsection
